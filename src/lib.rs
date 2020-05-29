ba71f15a11629a0d5f0280f3e5c30bff6ecddf9b
tuxxy <me@johnpacific.com>
2020-05-30 01:52:34 +0200

    Add rust project

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![no_std]
+#![forbid(unsafe_code)]
+
+/// This library implements NSA's lightweight block cipher SPECK.
+/// The formal specification of SPECK can be found: https://eprint.iacr.org/2013/404.pdf
+///
+/// The SPECK parameters can be found in Table 4.1 in the above paper.
+
+#[cfg(test)]
+mod tests {
+}
